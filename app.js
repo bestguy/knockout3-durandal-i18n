33562cd33b5dd6e2b3a28d9f52b62362020fe856
Gary Thomas <gary.thomas@citrix.com>
2014-06-09 12:11:49 -0700

    Remove Jade

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ var express = require('express');
 
 var app = express();
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-
 app.use(express.logger());
 app.use(express.static(__dirname + '/public'));
 
