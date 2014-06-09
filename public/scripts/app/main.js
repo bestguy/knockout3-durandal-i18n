da563a85c9b8d770c8a1c8ad7c1344093c319044
Gary Thomas <gary.thomas@citrix.com>
2014-06-09 12:12:39 -0700

    Add i18n

--- public/scripts/app/main.js
+++ public/scripts/app/main.js
@@ -1,9 +1,10 @@
 ﻿requirejs.config({
     paths: {
         'durandal': '../lib/durandal/js',
+        'i18next': '../lib/i18next/i18next.amd-1.7.3',
         'jquery': '../lib/jquery/jquery-1.9.1',
         'jquery.bootstrap': '../lib/bootstrap/bootstrap.min',
-        'knockout': '../lib/knockout/knockout-3.0.0',
+        'knockout': '../lib/knockout/knockout-3.1.0',
         'knockout.punches': '../lib/knockout/knockout.punches',
         'plugins': '../lib/durandal/js/plugins',
         'text': '../lib/require/text',
@@ -16,12 +17,25 @@
 
 define(function (require) {
 
+    // ### I18N
+    var i18next = require('i18next');
+    i18next.init({
+        fallbackLng: 'en',
+        resGetPath: '/scripts/app/locales/__lng__/__ns__.json'
+    });
+
     // Knockout
     var ko = require('knockout');
 
     // Enables cool Angular-style functionality:
     require('knockout.punches');
     ko.punches.enableAll();
+    ko.punches.attributeInterpolationMarkup.enable();
+
+    // I18N Binder
+    ko.filters['i18n'] = function (key) {
+        return i18next.t(key);
+    };
 
     // Example handlebars text filter to repeat a value `{{ value | repeat: n }}`
     ko.filters.repeat = function(value, n) {
